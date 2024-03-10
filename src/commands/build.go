cb72d15fdb1d24466aa173b467118fe77d84f1f4
hugos68 <hugokorteapple@gmail.com>
2024-03-10 23:03:44 +0100

    Markdown now

--- src/commands/build.go
+++ src/commands/build.go
@@ -1,12 +1,108 @@
 package commands
 
 import (
-	"fmt"
-	"stascii/src/utility"
+	"os"
+	"path/filepath"
+	"stamd/src/utility"
+	"strings"
+
+	"github.com/gomarkdown/markdown"
+	"github.com/gomarkdown/markdown/ast"
+	"github.com/gomarkdown/markdown/html"
+	"github.com/gomarkdown/markdown/parser"
 )
 
 func BuildCommand() {
 	config := utility.GetConfig()
+	pages := GetPages(config)
 
-	fmt.Println(config);
+	if len(pages) == 0 {
+		panic("No valid pages found in \"" + config.PagesDir + "\".")
+	}
+	document := ConvertPagesToDocuments(pages, config)
+	WriteDocumentsToDisk(document, config)
+}
+
+func GetPages(config utility.Config) []Page {
+	pagesDir := config.PagesDir
+	pages := []Page{}
+
+	filepath.Walk(pagesDir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			panic(err)
+		}
+
+		if filepath.Ext(path) != ".md" {
+			return nil
+		}
+
+		pages = append(pages, Page{
+			Path: path,
+		})
+
+		return nil
+	})
+
+	return pages
+}
+
+func ConvertPagesToDocuments(pages []Page, config utility.Config) []Document {
+	documents := []Document{}
+
+	extensions := parser.CommonExtensions | parser.AutoHeadingIDs | parser.NoEmptyLineBeforeBlock | parser.Autolink
+	p := parser.NewWithExtensions(extensions)
+
+	for _, page := range pages {
+		content, err := os.ReadFile(page.Path)
+
+		if err != nil {
+			panic(err)
+		}
+
+		node := p.Parse(content)
+
+		documents = append(documents, Document{
+			Path: page.Path,
+			Node: node,
+		})
+	}
+
+	return documents
+}
+
+func WriteDocumentsToDisk(documents []Document, config utility.Config) {
+
+	if _, err := os.Stat(config.OutDir); os.IsNotExist(err) {
+		os.Mkdir(config.OutDir, os.ModePerm)
+	}
+
+	htmlFlags := html.CommonFlags | html.HrefTargetBlank
+	opts := html.RendererOptions{Flags: htmlFlags}
+	renderer := html.NewRenderer(opts)
+
+	for _, document := range documents {
+		outPath := strings.Replace(document.Path, config.PagesDir, config.OutDir, 1)
+		outPath = strings.Replace(outPath, ".md", ".html", 1)
+
+		if _, err := os.Stat(filepath.Dir(outPath)); os.IsNotExist(err) {
+			os.MkdirAll(filepath.Dir(outPath), os.ModePerm)
+		}
+
+		html := markdown.Render(document.Node, renderer)
+
+		writeFileErr := os.WriteFile(outPath, html, os.ModePerm)
+
+		if writeFileErr != nil {
+			panic(writeFileErr)
+		}
+	}
+}
+
+type Page struct {
+	Path string
+}
+
+type Document struct {
+	Path string
+	Node ast.Node
 }
--- src/utility/config.go
+++ src/utility/config.go
@@ -6,14 +6,17 @@ import (
 	"os"
 )
 
+const ConfigFileName = "stamd.config.json"
+
 type Config struct {
 	Name        string
 	Description string
+	PagesDir    string
 	OutDir      string
 }
 
 func GetConfig() Config {
-	configJson, readErr := os.ReadFile("stascii.config.json")
+	configJson, readErr := os.ReadFile(ConfigFileName)
 
 	if readErr != nil {
 		panic(readErr)
@@ -24,18 +27,22 @@ func GetConfig() Config {
 	unmarshalErr := json.Unmarshal(configJson, &config)
 
 	if unmarshalErr != nil {
-		fmt.Println("Error parsing stascii.config.json:", unmarshalErr)
+		fmt.Println("Error parsing ", ConfigFileName, ":", unmarshalErr)
 		os.Exit(1)
 	}
 
 	if config.Name == "" {
-		fmt.Println("Name is required in stascii.config.json")
+		fmt.Println("Name is required in ", ConfigFileName)
 		os.Exit(1)
 	}
 
 	if config.Description == "" {
-		fmt.Println("Description is required in stascii.config.json")
+		fmt.Println("Description is required ", ConfigFileName)
 		os.Exit(1)
+	}
+
+	if config.PagesDir == "" {
+		config.PagesDir = "pages"
 	}
 
 	if config.OutDir == "" {
